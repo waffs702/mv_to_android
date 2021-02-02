0dedc87ec24e0683f52cf800970e33eb12ba18dd
waffs702 <waffs702>
2021-02-02 10:17:53 +0900

    SNSシェア追加

--- app/src/main/java/com/example/mv_to_mobile_android/ScriptHandler.java
+++ app/src/main/java/com/example/mv_to_mobile_android/ScriptHandler.java
@@ -1,11 +1,19 @@
 package com.example.mv_to_mobile_android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
-import android.os.Handler;
+import java.io.ByteArrayOutputStream;
 
 public class ScriptHandler {
     private static final String TAG = "ScriptHandler";
@@ -84,5 +92,40 @@ public class ScriptHandler {
         });
     }
 
+    @JavascriptInterface
+    public void shareSNS(String shareText, String imageData) {
+        this.handler.post(new Runnable() {
+            @Override
+            public void run() {
+                showShareSheet(shareText, imageData);
+            }
+        });
+    }
+
+    private void showShareSheet(String shareText, String imageData) {
+        Uri imageUri = null;
+        if (imageData != null && !"".equals(imageData)) {
+            byte[] bytes = Base64.decode(imageData, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageUri = getImageUri(bitmap);
+        }
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+        if (imageUri != null) {
+            shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+            shareIntent.setType("image/jpeg");
+        }
+        this.activity.startActivity(Intent.createChooser(shareIntent, null));
+    }
+
+    private Uri getImageUri(Bitmap inImage){
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        Context context = MyApplication.getInstance();
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage,"title",null);
+        return Uri.parse(path);
+    }
 
 }
